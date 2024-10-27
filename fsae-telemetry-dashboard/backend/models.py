78d4811964f91969e432469066f4617ce1a6b6df
maximillionare2030 <maxtrinh4@gmail.com>
2024-10-27 14:18:51 -0700

    Added comments to models.py for extra fields to be added

--- fsae-telemetry-dashboard/backend/models.py
+++ fsae-telemetry-dashboard/backend/models.py
@@ -5,6 +5,7 @@ class MotorData(BaseModel):
     motorSPD: float = Field(description="Motor speed in m/s")
     motorTMP: float = Field(description="Motor temperature in Fahrenheit")
 
+
 class ControllerData(BaseModel):
     controllerTMP: float = Field(description="Controller temperature in Fahrenheit")
 
@@ -12,4 +13,15 @@ class BatteryData(BaseModel):
     batteryVOLT: float = Field(description="Battery voltage in Volts")
     batteryTEMP: float = Field(description="Battery temperature in Fahrenheit")
     batteryCURR: float = Field(description="Battery current draw in Amps")
+
+    """
+    batteryCCL
+    highestTEMP
+    lowestTEMP
+    relayState
+    batterySOC
+    batteryRestistance
+    openVoltage
+    Amphours
+    """
     
