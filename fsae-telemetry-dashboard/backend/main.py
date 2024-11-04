c19609a16545cf87f17529e53cf2124b1689373d
Jasmine <jasmineqw45@gmail.com>
2024-11-04 11:17:04 -0800

    data successfully writes to the db

--- fsae-telemetry-dashboard/backend/main.py
+++ fsae-telemetry-dashboard/backend/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes.influx_routes import influx_bp 
+from routes.influx_routes import influx_bp
  
 
 app = FastAPI()
